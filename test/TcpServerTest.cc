e5c72d5779522a277148753765c14783bee599c7
HxGned <dengxionghui@hotmail.com>
2022-10-01 09:18:58 +0800

    MyReactor: add test for TcpServer and TcpClient

---
+++ test/TcpServerTest.cc
@@ -0,0 +1,48 @@
+#include "TcpServer.hh"
+#include "EventLoop.hh"
+#include "Log.hh"
+#include "HexUtils.hh"
+
+#include <iostream>
+
+using namespace std;
+using namespace net;
+using namespace base;
+
+void ReadClientData(const TcpConnectionPtr& conn, ByteBuffer* buf, Timestamp ts)
+{
+    LOG_INFO("ReadClientData begin! conn: [%s], time: [%s]", conn->GetConnName().c_str(), ts.ConvertToString().c_str());
+
+    char sendBuf[512] = {0};
+
+    size_t recvBytes = buf->ReadableBytes();
+    LOG_INFO("recv bytes: [%lu]", recvBytes);
+    memcpy(sendBuf, buf->ReadBegin(), recvBytes);
+    util::hexdump(sendBuf, recvBytes);
+    buf->RetrieveAll();
+
+    conn->Write(sendBuf, recvBytes);
+    LOG_INFO("ReadClientData end");
+}
+
+void NotifyWriteDone(const TcpConnectionPtr& conn)
+{
+    LOG_INFO("conn [%s] write done!", conn->GetConnName().c_str());
+}
+
+int main(int argc, char *argv[])
+{
+    cout << "TcpServer start!" << endl;
+    EventLoop loop;
+    INetAddr listenAddr("0.0.0.0", 9090);
+
+    TcpServer myServer(&loop, listenAddr, "myServer");
+    myServer.start();
+    myServer.setMessageReadCallback(bind(ReadClientData, placeholders::_1, placeholders::_2, placeholders::_3));
+    myServer.setMessageWriteCompleteCallback(bind(NotifyWriteDone, std::placeholders::_1));
+
+    loop.loop();
+
+    cout << "TcpServer end!" << endl;
+    return 0;
+}
